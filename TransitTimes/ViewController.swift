030345545215c05cbaab62ff54d4a9956166d228
John Bruce <jbruce2112@gmail.com>
2019-03-02 11:43:32 -0800

    Initial Commit

---
+++ TransitTimes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransitTimes
+//
+//  Created by John on 3/2/19.
+//  Copyright © 2019 Bruce32. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
